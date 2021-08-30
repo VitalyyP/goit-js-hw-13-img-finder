3be25ad35d5c3c53b42ccbbd812d54ffd9c74e09
Vitalyy Pikozh <wisart@gmail.com>
2021-08-30 23:35:10 +0300

    add scrollup button

--- src/index.js
+++ src/index.js
@@ -1,5 +1,65 @@
-import './sass/main.scss';
+import FetchImages from './js/apiService';
+import createCard from './templates/card.hbs';
+import * as basicLightbox from 'basiclightbox';
 
+import topFunction from './js/scroll';
 
+import '../node_modules/basiclightbox/dist/basicLightbox.min.css';
+import './style.css';
 
+const refForm = document.querySelector('#search-form');
+const refGalleryList = document.querySelector('.gallery__list');
+const refGallery = document.querySelector('.gallery');
+const refBtn = document.querySelector('.btn__load-more');
+const refScrollUp = document.querySelector('.scrollup');
 
+const newImage = new FetchImages();
+let currentSearchValue = '';
+
+refForm.addEventListener('submit', makeGallery);
+refBtn.addEventListener('click', addImagesAndScroll);
+refGalleryList.addEventListener('click', openImgInModal);
+refScrollUp.addEventListener('click', topFunction);
+refBtn.style.visibility = 'hidden';
+
+function makeGallery(e) {
+  e.preventDefault();
+  newImage.page = 1;
+  refBtn.style.visibility = 'hidden';
+  getQueryFromInput(e);
+  newImage.searchQuery = currentSearchValue;
+  addImagestoGallery();
+}
+
+function getQueryFromInput(e) {
+  currentSearchValue = e.currentTarget.elements.query.value;
+}
+
+function addImagestoGallery() {
+  newImage.fetchImage().then(data => {
+    refGalleryList.innerHTML = createCard(data.hits);
+    if (data.hits.length > 11) {
+      refBtn.style.visibility = 'visible';
+    }
+  });
+}
+
+function addImagesAndScroll() {
+  newImage.fetchImage().then(data => {
+    refGalleryList.insertAdjacentHTML('beforeend', createCard(data.hits));
+    refGallery.scrollIntoView({
+      behavior: 'smooth',
+      block: 'end',
+    });
+  });
+}
+
+function openImgInModal(e) {
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img src=${e.target.dataset.src} width="800" height="600">
+`);
+  instance.show();
+}
---
+++ src/js/apiService.js
@@ -0,0 +1,28 @@
+// import axios from 'axios';
+
+const BASE_URL = 'https://pixabay.com/api/';
+const KEY = '23134981-bd961700c800aa88c1e42f5d0';
+
+export default class fetchImages {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  fetchImage(searchQuery) {
+    const url = `${BASE_URL}?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${KEY}`;
+
+    return fetch(url)
+      .then(response => response.json())
+      .then(this.page +=1)
+      .catch(err => console.log(err));
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
